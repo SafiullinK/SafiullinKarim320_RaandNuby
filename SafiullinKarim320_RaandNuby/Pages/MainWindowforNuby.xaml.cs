e1a1976610fa57f3d3ff48ca56cf8cce37b2321b
Карим Сафиуллин <Karim.safiullin2302@gmail.com>
2023-10-26 10:11:51 +0300

    сделал лист вью и добавление ра и нуби

--- SafiullinKarim320_RaandNuby/Pages/MainWindowforNuby.xaml.cs
+++ SafiullinKarim320_RaandNuby/Pages/MainWindowforNuby.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using SafiullinKarim320_RaandNuby.DB;
 
 namespace SafiullinKarim320_RaandNuby.Pages
 {
@@ -20,9 +24,60 @@ namespace SafiullinKarim320_RaandNuby.Pages
     /// </summary>
     public partial class MainWindowforNuby : Page
     {
+        public static List<Sobachka_Nuby> nuby { get; set; }
         public MainWindowforNuby()
         {
             InitializeComponent();
+            nuby = new List<Sobachka_Nuby>(Connection.raAndNubyEntities.Sobachka_Nuby.ToList());
+            this.DataContext = this;
+            NubyLv.ItemsSource = new List<Sobachka_Nuby>(Connection.raAndNubyEntities.Sobachka_Nuby.ToList());
+
+        }
+
+
+        Sobachka_Nuby nuby1 = new Sobachka_Nuby();
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpg|*.jpg|*.jpeg|*.jpeg"
+            };
+            if (openFileDialog.ShowDialog().GetValueOrDefault())
+            {
+                nuby1.Image = File.ReadAllBytes(openFileDialog.FileName);
+                TestImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void AddNuby_Click(object sender, RoutedEventArgs e)
+        {
+            nuby1.deystv = deystv.Text;
+            Connection.raAndNubyEntities.Sobachka_Nuby.Add(nuby1);
+            Connection.raAndNubyEntities.SaveChanges();
+            NubyLv.ItemsSource = new List<Sobachka_Nuby>(Connection.raAndNubyEntities.Sobachka_Nuby.ToList());
+        }
+
+        private void AddRa_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainWindowforRa());
+            MessageBox.Show("Добро пожаловать к Нуби Деля!!");
+        }
+
+        private void FiltrTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+                if(FiltrTb.Text.Length > 0)
+                NubyLv.ItemsSource = Connection.raAndNubyEntities.Kotenok_Ra.Where(i => i.deystv.Contains(FiltrTb.Text.Trim())).ToList();
+                else
+                NubyLv.ItemsSource = new List<Sobachka_Nuby>(Connection.raAndNubyEntities.Sobachka_Nuby.ToList());
+        }
+
+        private void RaOrNubyCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(RaOrNubyCb.SelectedIndex == 0)
+            {
+               MessageBox.Show("Кто это")
+            }
         }
     }
 }
--- SafiullinKarim320_RaandNuby/Pages/MainWindowforRa.xaml.cs
+++ SafiullinKarim320_RaandNuby/Pages/MainWindowforRa.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using SafiullinKarim320_RaandNuby.DB;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +22,50 @@ namespace SafiullinKarim320_RaandNuby.Pages
     /// Логика взаимодействия для MainWindowforRa.xaml
     /// </summary>
     public partial class MainWindowforRa : Page
-    {
+    {   public static List<Kotenok_Ra> ra = new List<Kotenok_Ra>();
         public MainWindowforRa()
         {
             InitializeComponent();
+            this.DataContext = this;
+            RaLv.ItemsSource = new List<Kotenok_Ra>(Connection.raAndNubyEntities.Kotenok_Ra.ToList());
+
+        }
+
+        Kotenok_Ra ra1 = new Kotenok_Ra();
+        private void AddRa_Click(object sender, RoutedEventArgs e)
+        {
+            ra1.deystv = deystv.Text;
+            Connection.raAndNubyEntities.Kotenok_Ra.Add(ra1);
+            Connection.raAndNubyEntities.SaveChanges();
+            RaLv.ItemsSource = new List<Kotenok_Ra>(Connection.raAndNubyEntities.Kotenok_Ra.ToList());
+        }
+
+        private void AddNuby_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainWindowforNuby());
+            MessageBox.Show("Добро пожаловать к Ра Андрей!!"); 
+        }
+
+        private void AddImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpg|*.jpg|*.jpeg|*.jpeg"
+            };
+            if (openFileDialog.ShowDialog().GetValueOrDefault())
+            {
+                ra1.Image = File.ReadAllBytes(openFileDialog.FileName);
+                TestImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void FiltrTb_TextChanged(object sender, TextChangedEventArgs e)
+        { 
+            if (FiltrTb.Text.Length > 0) 
+            RaLv.ItemsSource = Connection.raAndNubyEntities.Kotenok_Ra.Where(i => i.deystv.Contains(FiltrTb.Text.Trim())).ToList(); 
+            else
+            RaLv.ItemsSource = new List<Kotenok_Ra>(Connection.raAndNubyEntities.Kotenok_Ra.ToList());
+
         }
     }
 }
